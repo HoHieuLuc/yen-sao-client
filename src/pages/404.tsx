fbc597cc3fe698465878c9f721ea2fee6cdbe84f
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-26 22:12:57 +0700

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,12 @@
+import GenericError from '../components/Utils/Errors/GenericError';
 
 const NotFoundPage = () => {
     return (
-        <div>404</div>
+        <GenericError
+            statusCode={404}
+            title='Trang này không tồn tại'
+            description='Trang bạn đang truy cập không tồn tại, hãy kiểm tra lại đường dẫn.'
+        />
     );
 };
 
