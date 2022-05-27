2089aa56129857ae188b6b937f1db69705626c1d
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-28 01:11:28 +0700

    add featured san pham

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
-import SanPhamList from '../components/SanPham/SanPhamList';
-import { Divider, Stack } from '@mantine/core';
+import { useScrollIntoView } from '@mantine/hooks';
+
+import FeaturedSanPham from '../components/SanPham/Featured/FeaturedSanPham';
+import SanPhamList from '../components/SanPham/List/SanPhamList';
+import AppAffix from '../components/Utils/Affix/AppAffix';
 import About from '../components/About/About';
+import { Stack } from '@mantine/core';
 
 import { pageHooks, pageService, sanPhamService } from '../graphql/queries';
 import { addApolloState, initializeApollo } from '../graphql/client';
@@ -9,6 +13,10 @@ import { GetServerSideProps } from 'next';
 
 export default function HomePage() {
     const { data } = pageHooks.useAllPages();
+
+    const scrollToSanPhamList = useScrollIntoView<HTMLDivElement>({ offset: 100 });
+    const scrollToFeaturedSanPhams = useScrollIntoView<HTMLDivElement>({ offset: 50 });
+
     return (
         <Stack spacing='xs'>
             {data && (
@@ -16,8 +24,28 @@ export default function HomePage() {
                     <About data={data} />
                 </section>
             )}
-            <Divider />
-            <SanPhamList />
+            <div ref={scrollToFeaturedSanPhams.targetRef}>
+                <FeaturedSanPham />
+            </div>
+            <div ref={scrollToSanPhamList.targetRef}>
+                <SanPhamList />
+            </div>
+            <AppAffix
+                label='Tất cả sản phẩm'
+                onClick={() => scrollToSanPhamList.scrollIntoView()}
+                mounted={true}
+                position={{ bottom: 20, right: 20 }}
+                variant='gradient'
+                gradient={{ from: 'violet', to: 'blue', deg: 90 }}
+            />
+            <AppAffix
+                label='Sản phẩm tiêu biểu'
+                onClick={() => scrollToFeaturedSanPhams.scrollIntoView()}
+                mounted={true}
+                position={{ bottom: 20, right: 200 }}
+                variant='gradient'
+                gradient={{ from: 'violet', to: 'blue', deg: 90 }}
+            />
         </Stack>
     );
 }
@@ -30,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         limit: 12,
         search: parseString(query.search)
     });
+    await sanPhamService.getFeatured(client);
     await pageService.getAll(client);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return addApolloState(client, {
