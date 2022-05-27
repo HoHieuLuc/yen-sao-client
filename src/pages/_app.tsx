302de2f74e2203e1d89fd8088672e38a8c7896cc
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-28 01:12:04 +0700

    update page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,7 +57,7 @@ export default function App(props: Props) {
     return (
         <>
             <Head>
-                <title>Yến Sào MS.Tưởng</title>
+                <title>Yến Sào Ms. Tưởng</title>
                 <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
                 <link rel='shortcut icon' href='/favicon.ico' />
             </Head>
