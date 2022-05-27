62573361666b1be345f66eba563c42b585335c43
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-28 01:10:44 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -12,5 +12,6 @@ module.exports = withBundleAnalyzer({
 module.exports = {
     images: {
         domains: ['res.cloudinary.com', 'media.discordapp.net'],
+        deviceSizes: [576, 768, 992, 1200, 1400]
     },
 }
