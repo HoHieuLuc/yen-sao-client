762ade89461975107f4328c6d978623cf5d5c62e
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-28 16:57:42 +0700

    update footer style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 import useStyles from './Footer.styles';
 
-import { Text, Container, Image, Grid, Box } from '@mantine/core';
+import { Text, Container, Grid, Box } from '@mantine/core';
+import Image from 'next/image';
 
 import { AllPage } from '../../types';
+
 
 interface Props {
     data: AllPage;
@@ -13,16 +15,21 @@ const AppFooter = ({ data }: Props) => {
     return (
         <Box className={classes.footer}>
             <Container className={classes.inner}>
-                <Image
-                    src='/logo.png'
-                    alt='logo'
-                    fit='contain'
-                    width={60}
-                />
+                <div style={{ width: '4rem', height: '4rem' }}>
+                    <Image
+                        alt='logo'
+                        src='/logo.png'
+                        layout='responsive'
+                        width='100%'
+                        height='100%'
+                        objectFit='scale-down'
+                        sizes='100%'
+                    />
+                </div>
                 <div style={{ marginLeft: 'auto' }}>
                     <Grid justify='center'>
-                        <Grid.Col span={2}>Địa chỉ:</Grid.Col>
-                        <Grid.Col span={10}>
+                        <Grid.Col span={4} md={2}>Địa chỉ:</Grid.Col>
+                        <Grid.Col span={8} md={10}>
                             {data.page.address && data.page.address.content.value.length > 0
                                 ? data.page.address.content.value.map(
                                     (item, index) => (
@@ -31,8 +38,8 @@ const AppFooter = ({ data }: Props) => {
                                 : 'Địa chỉ chưa được cập nhật'
                             }
                         </Grid.Col>
-                        <Grid.Col span={2}>Số điện thoại:</Grid.Col>
-                        <Grid.Col span={10}>
+                        <Grid.Col span={4} md={2}>Số điện thoại:</Grid.Col>
+                        <Grid.Col span={8} md={10}>
                             {data.page.phone && data.page.phone.content.value.length > 0
                                 ? data.page.phone.content.value.map(
                                     (item, index) => (
