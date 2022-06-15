d73da6eb0f6c8abd086b104513f96f2f1928a9f6
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-15 21:27:27 +0700

    add cam nang types

---
+++ src/types/cam-nang.ts
@@ -0,0 +1,31 @@
+import { PageInfo, PaginateVars } from './paginate';
+
+export interface CamNang {
+    id: string;
+    tieuDe: string;
+    noiDung: string;
+    isPublic: boolean;
+    slug: string;
+    anhDaiDien: string;
+    createdAt: number;
+    updatedAt: number;
+}
+
+export interface AllCamNangs {
+    camNang: {
+        all: {
+            docs: Array<CamNang>;
+            pageInfo: PageInfo;
+        }
+    }
+}
+
+export interface AllCamNangsVars extends PaginateVars {
+    search: string;
+}
+
+export interface CamNangBySlug {
+    camNang: {
+        bySlug: CamNang;
+    }
+}
