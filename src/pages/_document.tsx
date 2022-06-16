4a86edfe7f8a505e191148e20d236092ead4507d
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-16 14:03:49 +0700

    sorry

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class _Document extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                     <link
-                        href='https://fonts.googleapis.com/css2?family=Montserrat&display=optional'
+                        href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap'
                         rel='stylesheet'
                     />
                 </Head>
