0da5ac94425aceb9df1b05b434b6c86a49e12f6f
Ho Hieu Luc <kingofika123@gmail.com>
2022-08-07 11:14:18 +0700

    declare return type for addApolloState

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -7,6 +7,7 @@ import isEqual from 'lodash/isEqual';
 import { AppProps } from 'next/app';
 import appConfig from '../config';
 import merge from 'deepmerge';
+import { GetServerSidePropsResult } from 'next';
 
 export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
 
@@ -82,7 +83,7 @@ export function initializeApollo(
 export function addApolloState(
     client: ApolloClient<NormalizedCacheObject>,
     pageProps: AppProps['pageProps']
-) {
+): GetServerSidePropsResult<{ [key: string]: unknown; }> {
     if (pageProps?.props) {
         pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
     }
